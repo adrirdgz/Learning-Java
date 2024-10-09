fa09a4efd33301dc3c9cebdb9aac6dd7955c9933
Adrian Rodriguez Diez <adrian.r.d12389@gmail.com>
2024-10-09 16:09:01 +0200

    Terminado types

--- src/main/java/com/campusdual/types/MinMaxValues.java
+++ src/main/java/com/campusdual/types/MinMaxValues.java
@@ -1,4 +1,25 @@
 package com.campusdual.types;
 
 public class MinMaxValues {
+    public static void main(String[] args) {
+
+        System.out.println("Minimum byte values: " + Byte.MIN_VALUE);
+        System.out.println("Maximum byte value " + Byte.MAX_VALUE);
+
+        System.out.println("Minimum short values: " + Short.MIN_VALUE);
+        System.out.println("Maximum short value " + Short.MAX_VALUE);
+
+        System.out.println("Minimum integer values: " + Integer.MIN_VALUE);
+        System.out.println("Maximum integer value " + Integer.MAX_VALUE);
+
+        System.out.println("Minimum long values: " + Long.MIN_VALUE);
+        System.out.println("Maximum long value " + Long.MAX_VALUE);
+
+        System.out.println("Minimum float values: " + Float.MIN_VALUE);
+        System.out.println("Maximum float value " + Float.MAX_VALUE);
+
+        System.out.println("Minimum double values: " + Double.MIN_VALUE);
+        System.out.println("Maximum double value " + Double.MAX_VALUE);
+
+    }
 }
