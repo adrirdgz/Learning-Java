8c1ecd504d5bca45afcfe7de2a709cc83ead137e
Adrian Rodriguez Diez <adrian.r.d12389@gmail.com>
2024-10-29 09:52:59 +0100

    Terminado exceptions

---
+++ src/main/java/com/campusdual/exceptions/Exceptions.java
@@ -0,0 +1,29 @@
+package com.campusdual.exceptions;
+
+public class Exceptions {
+
+    public static int divisionByNumber(int a, int b) throws ByZeroException{
+        if (b==0){
+            throw new ByZeroException("Cannot perform a division by 0");
+        }else {
+            return a/b;
+        }
+    }
+
+    public static void main(String[] args) {
+        //int dividend = 3, divisor = 1;
+        //int res = Exceptions.divisionByNumber(dividend, divisor);
+        //System.out.println(res);
+        //String greetigs = "Hello there!";
+        //System.out.println(Integer.parseInt(greetigs));
+
+        int dividend = 3, divisor = 0;
+        try{
+            int res = Exceptions.divisionByNumber(dividend, divisor);
+        }catch (ByZeroException e){
+            System.err.println("Program failed. " + e.getMessage());
+            e.printStackTrace();
+        }
+        System.out.println("Program finished");
+    }
+}
